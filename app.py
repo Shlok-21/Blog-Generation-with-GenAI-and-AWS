3f6b3ae9f1110db5d9de033424fe33c4e5b4dff5
Shlok-21 <shlokshivkar21@gmail.com>
2024-08-05 01:22:12 +0530

    lambda working, created basic flask, todo: add functionality, display blog, show blog

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/blog', methods = ['POST'])
+def generate_blog():
+    blog_topic = request.form['blogTitle']
+    blog_content = f'Generated content for blog title {blog_topic}'
+    return render_template('result.html', blog_title=blog_topic, blog_content=blog_content)
 
 
 if __name__ == '__main__':
