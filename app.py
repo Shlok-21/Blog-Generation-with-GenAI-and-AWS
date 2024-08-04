985dafc087b735d7dba11aa61ab403726bb38122
Shlok-21 <shlokshivkar21@gmail.com>
2024-08-04 23:07:55 +0530

    completed the lambda function and added the layer

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import boto3
 import json
 import botocore.client
 import botocore.config
-import response
+from datetime import datetime
 
 def generate_blog(topic:str) -> str:
-   
     prompt = """ <s>[INST]Human: Write a 200 words blog on the topic {topic}
     Assistant:[/INST] """
-    
     body = {
         'prompt' : prompt,
         'mag_gen_len' : 512,
@@ -20,7 +18,7 @@ def generate_blog(topic:str) -> str:
         bedrock = boto3.client('bedrock-runtime',
                                 region_name = 'ap-south-1',
                                 config = botocore.config.Config(read_timeout = 300, retries = {'max_attempts':3}))
-        bedrock.invoke(body = json.dumps(body),
+        response = bedrock.invoke(body = json.dumps(body),
                        modelID = 'meta.llama3-8b-instruct-v1:0')
         
         response_content = response.get('body').read()
@@ -29,11 +27,35 @@ def generate_blog(topic:str) -> str:
         
         blog_details = response_data['generation']
         return blog_details
+    
     except Exception as e:
         print(f'Error generating the blog, {e}')
         return('')
-    
-# continue at 24:50
-# https://www.youtube.com/watch?v=3OP39y4dO_Y
 
+def save_blog_details_in_S3(s3_key, s3_bucket, blog):
+    s3 = boto3.client('s3')
+    try:
+        s3.put_object(Bucket = s3_bucket, Key = s3_key, Body = blog)
+        print(f'Blog saved to S3')
+    except Exception as e:
+        print(f'Error occured while saving Blog to S3 {e}')    
+
+
+def lambda_handler(event, context):
+    event = json.loads(event['body'])
+    blog_topic = event['blog_topic']
+    blog = generate_blog(topic = blog_topic)
+    
+    # Save the generated blog in S3 bucket
+    if blog:
+        current_time = datetime.now().strftime('%H%M%S')
+        s3_key = 'blog_output/{current_time}.txt'
+        s3_bucket_name = 'blog_generator'
+        save_blog_details_in_S3(s3_key=s3_key, s3_bucket= s3_bucket_name, blog = blog)
+    else:
+        print(f'No blog was generated.')
         
+    return {
+        'statusCode':200,
+        'body':json.dumps('Blog generated successfully.')
+    }
