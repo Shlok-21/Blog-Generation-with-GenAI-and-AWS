a39c3d42a346701d75fb419452de50c2faa2e5d1
Shlok-21 <shlokshivkar21@gmail.com>
2024-08-06 19:54:15 +0530

    pre fetch S3 data

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+import requests
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'shlokbloggenerator'
+prefix = 'blog_output/'
 
 app = Flask(__name__)
 
@@ -10,7 +16,47 @@ def main():
 def generate_blog():
     blog_topic = request.form['blogTitle']
     blog_content = f'Generated content for blog title {blog_topic}'
-    return render_template('result.html', blog_title=blog_topic, blog_content=blog_content)
+    
+    # URL of the AWS Lambda function
+    lambda_url = 'https://j7d10659ya.execute-api.ap-south-1.amazonaws.com/dev/blog-generation'
+
+    # The payload to send to the Lambda function
+    payload = {
+        "blog_topic": blog_topic,
+        }
+
+    # hit the API Gateway
+    
+    try:
+        response = requests.post(lambda_url, json = payload)
+        if response.status_code==200:
+            response_data = response.json()
+            response_status = jsonify(response_data)
+            print(response_status)
+        else:
+            print(jsonify({'error': 'Failed to invoke Lambda function', 'status_code': response.status_code}))
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+    
+    # add blog display here
+    return render_template('result.html', blog_title=blog_topic, blog_content=blog_topic)
+
+
+    # try:
+    #     # Sending POST request to the Lambda function
+    #     response = requests.post(lambda_url, json=payload)
+
+    #     # Check if the request was successful
+    #     if response.status_code == 200:
+    #         # Process the response from the Lambda function
+    #         response_data = response.json()
+    #         return jsonify(response_data)
+    #     else:
+    #         return jsonify({'error': 'Failed to invoke Lambda function', 'status_code': response.status_code})
+    # except Exception as e:
+    #     return jsonify({'error': str(e)})
+
 
 
 if __name__ == '__main__':
